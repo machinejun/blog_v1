e8e152cc658c73ffdca0954d330c3a556fc5ccb7
machinejun <akswns03@naver.com>
2022-06-15 16:03:27 +0900

    init project

---
+++ src/main/java/com/tencoding/blog/TenoblogApplication.java
@@ -0,0 +1,13 @@
+package com.tencoding.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TenoblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TenoblogApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/tencoding/blog/controller/BlogController.java
@@ -0,0 +1,22 @@
+package com.tencoding.blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * 스프링 com.tenoding.blog 패키지 이하를 스캔해서 모든 자바 파일을
+ * 메모리에 new 하는 것은 아니고 특정 어노테이션 붙어 있는 파일들을
+ * new 해서 IoC 스프링 컨테이너에서 관리한다.
+ */
+@RestController
+@RequestMapping("/test")
+public class BlogController {
+	
+	@GetMapping("/hello")
+	public String hello() {
+		return "<h1>hello Spring Boot</h1>";
+	}
+
+}
