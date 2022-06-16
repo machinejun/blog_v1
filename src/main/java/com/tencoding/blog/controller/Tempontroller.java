bcfde79a0befdbc9bfa9fa0307b85cd9324cf311
machinejun <akswns03@naver.com>
2022-06-16 10:14:15 +0900

    yml 파일 추가 설정
    jpa 설정 추가, jsp 설정 추가

---
+++ src/main/java/com/tencoding/blog/controller/Tempontroller.java
@@ -0,0 +1,18 @@
+package com.tencoding.blog.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Tempontroller {
+	
+	@GetMapping("/temp/home")
+	public String tempHome() {
+		// 파일 리턴 경로 : src/main/resources/static
+		// 리턴명 : /home.html
+		
+		System.out.println("tempHome()");
+		return "/home.html";
+	}
+
+}
